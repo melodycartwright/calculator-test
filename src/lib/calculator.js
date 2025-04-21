8c1c2537175e6ddc7eae9e3dc9e1c4d7c2918a74
Melody Cartwright <melody94c@gmail.com>
2025-04-22 00:28:44 +0200

    Initial commit with caluclator logic

---
+++ src/lib/calculator.js
@@ -0,0 +1,13 @@
+export const createCalculator = () => {
+    return {
+        add: (a, b) => a + b,
+        subtract: (a, b) => a - b,
+        multiply: (a, b) => a * b,
+        divide: (a, b) => {
+            if (b===0) {
+                throw new Error("Can't divide by zero");
+            }
+            return a / b;
+        }
+    };
+};
