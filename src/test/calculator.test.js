2b3f50f3b52b200213d7946d83ec57e8bd6da36f
Melody Cartwright <melody94c@gmail.com>
2025-04-22 00:52:03 +0200

    added describe and test for addition

--- src/test/calculator.test.js
+++ src/test/calculator.test.js
@@ -5,4 +5,11 @@ let calculator;
 
 beforeAll (() => {
     calculator = createCalculator();
-})
+});
+
+describe('Addition', () => {
+    test('adding 2 + 4 will equal 6', () => {
+        const result = calculator.add(2, 4);
+        expect (result).toBe(6)
+    });
+});
