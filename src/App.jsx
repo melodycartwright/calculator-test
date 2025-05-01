a359dac4a6cbf782503fa03f33058dd4683a4ccf
Melody Cartwright <melody94c@gmail.com>
2025-05-01 23:11:19 +0200

    Load token from localStorage on component mount

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,26 @@
-import { useState } from "react";
-import RegisterMovie from "./components/RegisterMovie"
-import MovieList from "./components/MovieList"
+import { useState, useEffect } from "react";
+import RegisterMovie from "./components/RegisterMovie";
+import MovieList from "./components/MovieList";
 
 function App() {
-  const [token, setToken] = useState(localStorage.getItem("jwtToken") || "");
- 
+  const [token, setToken] = useState("");
+
+
+  useEffect(() => {
+    const storedToken = localStorage.getItem("jwtToken");
+    if (storedToken) {
+      setToken(storedToken);
+    }
+  }, []);
 
   return (
-    <>
-      <div>
-<RegisterMovie token={token} setToken={setToken} />
-<MovieList token={token} />
-      </div>
-  
-    </>
+    <div className="container" style={{ maxWidth: "800px", margin: "0 auto", padding: "20px" }}>
+      <h1>Movie Registration System</h1>
+      <RegisterMovie token={token} setToken={setToken} />
+      <MovieList token={token} />
+    </div>
   );
 }
 
-export default App
+export default App;
+
